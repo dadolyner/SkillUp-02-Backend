e24926b6868599bf5b2bed2646a122f88b613522
dadolyner <skulj.david@gmail.com>
2022-05-28 19:14:30 +0200

    added modules and defining files

--- src/modules/auth/auth.repository.ts
+++ src/modules/auth/auth.repository.ts
@@ -35,6 +35,8 @@ export class AuthRepository extends Repository<Users> {
                 throw new InternalServerErrorException();
             }
         }
+
+        this.logger.verbose(`User with email: ${email} successfully registered!`);
     }
 
     //validate inserted password
