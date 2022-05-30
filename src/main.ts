d411d78f424680c209e2eb5762d6492426122c53
dadolyner <skulj.david@gmail.com>
2022-05-30 21:10:13 +0200

    enable cors requests

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 
 const bootstrap = async () => {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, { cors: true });
     await app.listen(3000);
 }
 bootstrap();
